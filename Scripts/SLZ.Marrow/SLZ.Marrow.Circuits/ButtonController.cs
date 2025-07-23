383418717400b4d2abdcbc3b9915b56b0bdd90a1
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-07-23 10:52:35 -0400

    Add missing fields to CircuitSocket and ButtonController (#14)
    * Add missing fields to CircuitSocket and ButtonController.
    
    * Added Reset method to CircuitSocket

--- Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/ButtonController.cs
+++ Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/ButtonController.cs
@@ -7,6 +7,34 @@ namespace SLZ.Marrow.Circuits
     {
         [SerializeField]
         private ButtonMode _buttonMode = ButtonMode.ClickUp;
+
+        private const float downThreshold = 0.9f;
+
+        private const float upThreshold = 0.1f;
+
+        private bool _charged;
+        
+        private bool _toggleCharged;
+
+        [Tooltip("Clip(s) to be played on button press")]
+        [SerializeField]
+        protected AudioClip[] _pressClips;
+
+        [Tooltip("Clip(s) to be played on button unpress")]
+        [SerializeField]
+        protected AudioClip[] _depressClips;
+
+        public ButtonMode buttonMode
+        {
+            get
+            {
+                return _buttonMode;
+            }
+            set
+            {
+            }
+        }
+
         public enum ButtonMode
         {
             ClickUp,
--- Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/CircuitSocket.cs
+++ Scripts/SLZ.Marrow/SLZ.Marrow.Circuits/CircuitSocket.cs
@@ -1,8 +1,34 @@
 using UnityEngine;
+using SLZ.Marrow;
 
 namespace SLZ.Marrow.Circuits
 {
     public class CircuitSocket : Circuit
     {
+        [SerializeField]
+        private Circuit _input;
+
+        [SerializeField]
+        protected Servo _servo;
+
+        [SerializeField]
+        protected Rigidbody _rb;
+
+        public Circuit input
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+            }
+        }
+        
+        protected virtual void Reset()
+        {
+            TryGetComponent(out _servo);
+            TryGetComponent(out _rb);
+        }
     }
 }
